c5222767b384cf6ed786e1604869cd8083eaa550
HovhannesGal <galstyanhovhannes20@gmail.com>
2020-10-23 19:55:17 +0300

    js

---
+++ javascript/lesson_2_data_tipes/lesson/index.js
@@ -0,0 +1,52 @@
+/*
+*JS data types
+*(tvyalneri tipery) 
+*/
+
+//dinamic(tuyl) & static(hastatun)
+
+
+//  Primitive types
+// 1 number (... , NaN , +-Infinity)
+// 2 string
+// 3 boolean (true, false)
+// 4 undefined
+// 5 null
+// +- 
+
+// "use strict"
+
+/**
+ * operators
+ */
+
+ // 1. unar (+, - )
+ // 2. binar (+, - , * , / , % , ) ,  (= , += , -= , *= , /= , %= , ...) , (> , < , >= , <= , == , != , === , !==) , (&& , || ) , (| , &)
+ // 3. ternar
+
+ // && 
+ // && (true && true) = true
+ // && (true && false) = false
+ // && (false && false) = true
+ // && (false && true) = true
+
+ // || 
+ // || (true  || true) = true
+ // || (true  || false) = true
+ // || (false || true) = true
+ // || (false || false) = false
+
+
+ var d = 10;
+ d = 5;
+ 
+
+ console.log(d)
+ 
+
+
+
+
+
+
+
